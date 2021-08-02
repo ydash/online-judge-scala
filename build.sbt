f03b87d95580ab2ab3879e836be0327f44864c37
ydash <52343777+ydash@users.noreply.github.com>
2021-08-02 23:51:12 +0900

    add testing framework and template spec for leetcode

--- build.sbt
+++ build.sbt
@@ -3,3 +3,6 @@ name := "online-judge-scala"
 version := "0.1"
 
 scalaVersion := "2.13.6"
+
+libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.9"
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % "test"
---
+++ src/test/scala/leetcode/LeetCodeSpec.scala
@@ -0,0 +1,7 @@
+package leetcode
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should
+import org.scalatest.prop.TableDrivenPropertyChecks
+
+abstract class LeetCodeSpec extends AnyFunSuite with TableDrivenPropertyChecks with should.Matchers
