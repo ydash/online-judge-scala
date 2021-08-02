d262f9959f4f87215a71a4ee369f579d83053e2f
ydash <52343777+ydash@users.noreply.github.com>
2021-08-03 02:30:39 +0900

    add solution of Two Sum

---
+++ src/main/scala/leetcode/codingchallenge2021/august/TwoSum.scala
@@ -0,0 +1,18 @@
+package leetcode.codingchallenge2021.august
+
+import scala.annotation.tailrec
+
+object TwoSum {
+  def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+    val sortedIndices = nums.indices.sortBy(i => nums(i))
+
+    @tailrec
+    def loop(i: Int, j: Int): Array[Int] = nums(sortedIndices(i)) + nums(sortedIndices(j)) match {
+      case x if x < target => loop(i + 1, j)
+      case x if x > target => loop(i, j - 1)
+      case _ => Array(sortedIndices(i), sortedIndices(j))
+    }
+
+    loop(0, nums.length - 1)
+  }
+}
