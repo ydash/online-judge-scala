b2ff705aca27ae2834592f021cbbece37b4ca48e
ydash <52343777+ydash@users.noreply.github.com>
2021-08-05 00:51:06 +0900

    add solution of Path Sum II

---
+++ src/main/scala/leetcode/codingchallenge2021/august/PathSumII.scala
@@ -0,0 +1,34 @@
+package leetcode.codingchallenge2021.august
+
+import util.TreeNode
+
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
+object PathSumII {
+
+  def pathSum(root: TreeNode, targetSum: Int): List[List[Int]] =
+    if (root == null) List.empty
+    else {
+      val answer = ListBuffer[List[Int]]()
+
+      val stack = new mutable.Stack[(Int, TreeNode, List[Int])]()
+      stack.push((root.value, root, List(root.value)))
+      while (stack.nonEmpty) {
+        val (sum, current, lst) = stack.pop()
+        if (sum == targetSum && current.left == null && current.right == null) answer += lst.reverse
+        else {
+          current.right match {
+            case null => ()
+            case r => stack.push((sum + r.value, r, r.value :: lst))
+          }
+          current.left match {
+            case null => ()
+            case l => stack.push((sum + l.value, l, l.value :: lst))
+          }
+        }
+      }
+
+      answer.toList
+    }
+}
