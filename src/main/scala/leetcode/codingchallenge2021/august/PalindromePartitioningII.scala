550e9207c6d409fe04e56f33746679ac0350882d
ydash <52343777+ydash@users.noreply.github.com>
2021-08-08 01:42:17 +0900

    optimize the code of Palindrome Partitioning II

--- src/main/scala/leetcode/codingchallenge2021/august/PalindromePartitioningII.scala
+++ src/main/scala/leetcode/codingchallenge2021/august/PalindromePartitioningII.scala
@@ -1,32 +1,21 @@
 package leetcode.codingchallenge2021.august
 
-import scala.annotation.tailrec
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
-
 object PalindromePartitioningII {
   def minCut(s: String): Int = {
-    val map = new mutable.HashMap[Int, ArrayBuffer[Int]]()
-    for (p <- s.indices) {
-      addIndicesOfPalindrome(s, p - 1, p + 1, map)
-      addIndicesOfPalindrome(s, p, p + 1, map)
+    val dp = Array.tabulate(s.length)(i => i)
+    for (i <- dp.indices) {
+      updateDp(dp, s, i, i)
+      updateDp(dp, s, i, i + 1)
     }
-
-    val dp = Array.fill(s.length)(0)
-    for (i <- 1 until dp.length) {
-      if (map.contains(i)) dp(i) = dp(i - 1) + 1 min map(i).map(j => if (j <= 0) 0 else dp(j - 1) + 1).min
-      else dp(i) = dp(i - 1) + 1
-    }
-
     dp.last
   }
 
-  @tailrec
-  def addIndicesOfPalindrome(str: String, left: Int, right: Int, map: mutable.HashMap[Int, ArrayBuffer[Int]]): Unit = {
-    if (left >= 0 && right < str.length && str(left) == str(right)) {
-      if (map.contains(right)) map(right) += left
-      else map.put(right, ArrayBuffer(left))
-      addIndicesOfPalindrome(str, left - 1, right + 1, map)
+  @scala.annotation.tailrec
+  def updateDp(dp: Array[Int], s: String, left: Int, right: Int): Unit = {
+    if (left >= 0 && right < s.length && s(left) == s(right)) {
+      if (left == 0) dp(right) = 0
+      else dp(right) = dp(right) min dp(left - 1) + 1
+      updateDp(dp, s, left - 1, right + 1)
     }
   }
 }
