538f7674de416a3fb4998aff56e282b76bc07a94
ydash <52343777+ydash@users.noreply.github.com>
2021-08-09 22:04:47 +0900

    make code little concise

--- src/main/scala/leetcode/codingchallenge2021/august/AddStrings.scala
+++ src/main/scala/leetcode/codingchallenge2021/august/AddStrings.scala
@@ -1,22 +1,16 @@
 package leetcode.codingchallenge2021.august
 
+import scala.util.chaining.scalaUtilChainingOps
 
 object AddStrings {
   def addStrings(num1: String, num2: String): String = adder(num1, num2)
 
-  def adder(num1: String, num2: String): String = {
-    @scala.annotation.tailrec
-    def adder(digits: Seq[(Char, Char)], carryUp: Int = 0, acc: List[Char] = List()): String = digits match {
-      case Vector() => (if (carryUp > 0) '1' :: acc else acc).mkString
-      case (x, y) +: rest =>
-        (x + y - '0' + carryUp).toChar match {
-          case n if '0' to '9' contains n =>
-            adder(rest, 0, n :: acc)
-          case n =>
-            adder(rest, 1, (n - 10).toChar :: acc)
-        }
-    }
-
-    adder(num1.reverse.zipAll(num2.reverse, '0', '0'))
-  }
+  def adder(num1: String, num2: String): String =
+    num1.reverse.zipAll(num2.reverse, '0', '0').foldLeft((List[Char](), 0)) { (acc, digits) =>
+      val (result, carryUp) = acc
+      (digits._1 + digits._2 - '0' + carryUp).toChar match {
+        case n if '0' to '9' contains n => (n :: result, 0)
+        case n => ((n - 10).toChar :: result, 1)
+      }
+    }.pipe { case (acc, c) => (if (c > 0) '1' :: acc else acc).mkString }
 }
