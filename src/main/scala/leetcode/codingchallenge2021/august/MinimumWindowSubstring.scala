72496e074315bc8cec4e7f9cf04b7a18532ef289
ydash <52343777+ydash@users.noreply.github.com>
2021-08-16 04:03:00 +0900

    make code more concise

--- src/main/scala/leetcode/codingchallenge2021/august/MinimumWindowSubstring.scala
+++ src/main/scala/leetcode/codingchallenge2021/august/MinimumWindowSubstring.scala
@@ -2,51 +2,35 @@ package leetcode.codingchallenge2021.august
 
 import scala.annotation.tailrec
 import scala.collection.mutable
+import scala.util.chaining.scalaUtilChainingOps
 
 object MinimumWindowSubstring {
 
   def minWindow(s: String, t: String): String = {
-    val countT = new mutable.HashMap[Char, Int]().withDefaultValue(0)
-    t.foreach(countT(_) -= 1)
-    sliding(s, 0, 0, Set.from(countT.keys), countT, None)
+    val count = new mutable.HashMap[Char, Int]().withDefaultValue(0)
+    t.foreach(count(_) += 1)
+    val filteredS = s.zipWithIndex.filter { case (c, _) => count.contains(c) }
+    sliding(0, 0, filteredS, count, None).map(range => s.substring(range.start, range.last + 1)).getOrElse("")
   }
 
   @tailrec
-  private def sliding(s: String,
-                      left: Int,
-                      right: Int, keys: Set[Char],
-                      redundant: mutable.Map[Char, Int],
-                      range: Option[Range]
-                     ): String =
-    if (right >= s.length) range.map(v => s.substring(v.start, v.last + 1)).getOrElse("")
+  private def sliding(left: Int, right: Int, seq: IndexedSeq[(Char, Int)],
+                      count: mutable.Map[Char, Int], acc: Option[Range]): Option[Range] =
+    if (right >= seq.length) acc
     else {
-      val c = s(right)
-      if (keys(c)) {
-        redundant(c) += 1
-        if (redundant(c) == 0) redundant.remove(c)
-
-        var l = left
-        while (l < right && (!keys(s(l)) || redundant(s(l)) > 0)) {
-          if (redundant(s(l)) > 0) {
-            redundant(s(l)) -= 1
-            if (redundant(s(l)) == 0) redundant.remove(s(l))
-          }
-          l += 1
-        }
-
-        val minRange =
-          if (redundant.isEmpty || redundant.values.forall(_ >= 0)) {
-            val tmp = l to right
-            range match {
-              case None => Some(tmp)
-              case Some(v) if tmp.length < v.length => Some(tmp)
-              case _ => range
-            }
-          } else range
-
-        sliding(s, l, right + 1, keys, redundant, minRange)
-      } else {
-        sliding(s, left, right + 1, keys, redundant, range)
+      val (c, i) = seq(right)
+      count(c) -= 1
+      if (count(c) == 0) count.remove(c)
+      var l = left
+      while (l < right && count(seq(l)._1) < 0) {
+        val lc = seq(l)._1
+        count(lc) += 1
+        if (count(lc) == 0) count.remove(lc)
+        l += 1
       }
+      val minRange = (seq(l)._2 to i).pipe(rng =>
+        if (count.values.forall(_ <= 0) && rng.length < acc.map(_.length).getOrElse(Int.MaxValue)) Some(rng) else acc
+      )
+      sliding(l, right + 1, seq, count, minRange)
     }
 }
