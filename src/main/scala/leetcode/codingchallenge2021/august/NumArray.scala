87c3b95b390131820c69d8267752e09640eeec47
ydash <52343777+ydash@users.noreply.github.com>
2021-08-17 00:07:44 +0900

    move case classes which is subtype of SegmentTree to companion object

--- src/main/scala/leetcode/codingchallenge2021/august/NumArray.scala
+++ src/main/scala/leetcode/codingchallenge2021/august/NumArray.scala
@@ -1,6 +1,6 @@
 package leetcode.codingchallenge2021.august
 
-import util.segmenttree.SegmentTree
+import util.SegmentTree
 
 class NumArray(_nums: Array[Int]) {
   private val st: SegmentTree = SegmentTree(_nums)
