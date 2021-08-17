32935878d62e7a5593d9909148b0f4fbb2a1e2ae
ydash <52343777+ydash@users.noreply.github.com>
2021-08-17 22:49:40 +0900

    add companion object

--- src/main/scala/util/TreeNode.scala
+++ src/main/scala/util/TreeNode.scala
@@ -1,9 +1,20 @@
 package util
 
+import scala.util.chaining.scalaUtilChainingOps
+
 class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
   var value: Int = _value
   var left: TreeNode = _left
   var right: TreeNode = _right
 
   override def toString: String = s"TreeNode($value,$left,$right)"
+}
+
+object TreeNode {
+  def apply(num: Int) = new TreeNode(num)
+
+  def apply(num: Int, left: TreeNode, right: TreeNode): TreeNode = TreeNode(num).tap { node =>
+    node.left = left
+    node.right = right
+  }
 }
