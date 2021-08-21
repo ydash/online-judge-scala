6ffe684aa01478c3f38a35343cba3b869f08be20
ydash <52343777+ydash@users.noreply.github.com>
2021-08-22 02:26:50 +0900

    make code more efficient

--- src/main/scala/leetcode/codingchallenge2021/august/SudokuSolver.scala
+++ src/main/scala/leetcode/codingchallenge2021/august/SudokuSolver.scala
@@ -2,27 +2,25 @@ package leetcode.codingchallenge2021.august
 
 object SudokuSolver {
   def solveSudoku(board: Array[Array[Char]]): Unit = {
-    val set = (for (i <- board.indices; j <- board.indices if board(i)(j) != '.') yield (i, j)).foldLeft(Set[String]()) { case (acc, (i, j)) =>
-      val x = board(i)(j)
-      acc + s"r($i,$x)" + s"c($j,$x)" + s"b(${i / 3},${j / 3},$x)"
-    }
-    val emptyCells = for (i <- board.indices; j <- board.indices if board(i)(j) == '.') yield (i, j)
-    solver(board, emptyCells, set)
+    solver(board)
   }
 
-  def solver(board: Array[Array[Char]], emptyCells: Seq[(Int, Int)], set: Set[String]): Boolean = emptyCells match {
-    case Nil => true
-    case (i, j) +: rest =>
-      for {
-        x <- '1' to '9'
-        r = s"r($i,$x)"
-        c = s"c($j,$x)"
-        b = s"b(${i / 3},${j / 3},$x)"
-        if !set.contains(r) && !set.contains(c) && !set.contains(b) && solver(board, rest, set + r + c + b)
-      } {
+  def validate(board: Array[Array[Char]], row: Int, col: Int, x: Char): Boolean =
+    board.indices.forall { i =>
+      board(row)(i) != x &&
+        board(i)(col) != x &&
+        board(3 * (row / 3) + i / 3)(3 * (col / 3) + i % 3) != x
+    }
+
+  def solver(board: Array[Array[Char]]): Boolean = {
+    for (i <- board.indices; j <- board.indices if board(i)(j) == '.') {
+      for (x <- '1' to '9' if validate(board, i, j, x)) {
         board(i)(j) = x
-        return true
+        if (solver(board)) return true
+        board(i)(j) = '.'
       }
-      false
+      return false
+    }
+    true
   }
 }
