38035497ffcf4de0575f866ba6c4f009b80abe6a
ydash <52343777+ydash@users.noreply.github.com>
2021-08-27 00:53:49 +0900

    make code conciese

--- src/main/scala/leetcode/codingchallenge2021/august/VerifyPreorderSerializationOfABinaryTree.scala
+++ src/main/scala/leetcode/codingchallenge2021/august/VerifyPreorderSerializationOfABinaryTree.scala
@@ -3,36 +3,15 @@ package leetcode.codingchallenge2021.august
 import scala.annotation.tailrec
 
 object VerifyPreorderSerializationOfABinaryTree {
-  private val NULL_POINTER = -1
 
-  def isValidSerialization(preorder: String): Boolean =
-    if (preorder == "#") true
-    else {
-      val nodes = preorder.split(',').map { str => if (str == "#") NULL_POINTER else str.toInt }.toList
-      nodes.head != NULL_POINTER && check(nodes.tail, List(nodes.head), List())
-    }
+  def isValidSerialization(preorder: String): Boolean = validate(preorder.split(','))
 
   @tailrec
-  private def check(nodes: List[Int], left: List[Int], right: List[Int]): Boolean = left match {
-    case Nil =>
-      right match {
-        case Nil =>
-          nodes match {
-            case Nil => true
-            case _   => false
-          }
-        case _ :: rt =>
-          nodes match {
-            case Nil                  => false
-            case NULL_POINTER :: tail => check(tail, left, rt)
-            case head :: tail         => check(tail, head :: left, rt)
-          }
-      }
-    case lh :: lt =>
-      nodes match {
-        case Nil                  => false
-        case NULL_POINTER :: tail => check(tail, lt, lh :: right)
-        case head :: tail         => check(tail, head :: lt, lh :: right)
-      }
-  }
+  private def validate(nodes: Seq[String], acc: Int = 1): Boolean =
+    nodes match {
+      case Nil           => acc == 0
+      case _ if acc <= 0 => false
+      case "#" +: tail   => validate(tail, acc - 1)
+      case _ +: tail     => validate(tail, acc + 1)
+    }
 }
