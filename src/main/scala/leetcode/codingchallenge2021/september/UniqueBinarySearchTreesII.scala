462f7a9358ea11b112366d3b070bd98dc323ccc4
ydash <52343777+ydash@users.noreply.github.com>
2021-09-02 20:41:55 +0900

    make code more concise

--- src/main/scala/leetcode/codingchallenge2021/september/UniqueBinarySearchTreesII.scala
+++ src/main/scala/leetcode/codingchallenge2021/september/UniqueBinarySearchTreesII.scala
@@ -2,37 +2,19 @@ package leetcode.codingchallenge2021.september
 
 import util.TreeNode
 
-import scala.annotation.tailrec
-import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 object UniqueBinarySearchTreesII {
-  def generateTrees(n: Int): List[TreeNode] = {
-    val result = mutable.Map[String, TreeNode]()
-    (1 to n).permutations.foreach { nums =>
-      val root = TreeNode(nums.head)
-      nums.drop(1).foreach(TreeNode.insert(root, _))
-      result += TreeNode.toString(root) -> root
-    }
-    result.values.toList
-  }
 
-  object TreeNode {
-    def apply(n: Int) = new TreeNode(n, null, null)
+  def generateTrees(n: Int): List[TreeNode] = generateTrees(1, n)
 
-    @tailrec
-    def insert(node: TreeNode, n: Int): Unit =
-      if (n < node.value) {
-        if (node.left == null) node.left = TreeNode(n)
-        else insert(node.left, n)
-      } else {
-        if (node.right == null) node.right = TreeNode(n)
-        else insert(node.right, n)
+  def generateTrees(start: Int, end: Int): List[TreeNode] =
+    if (start > end) List(null)
+    else {
+      val buffer = ListBuffer[TreeNode]()
+      for (i <- start to end) {
+        generateTrees(start, i - 1).foreach(l => generateTrees(i + 1, end).foreach(r => buffer += TreeNode(i, l, r)))
       }
-
-    def toString(node: TreeNode): String = {
-      val l = if (node.left == null) "null" else toString(node.left)
-      val r = if (node.right == null) "null" else toString(node.right)
-      s"(${node.value},$l,$r)"
+      buffer.toList
     }
-  }
 }
