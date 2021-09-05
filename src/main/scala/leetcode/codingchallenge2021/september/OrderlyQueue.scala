bb8d84ff510a0b828c2a630df6af65f583d4a07b
ydash <52343777+ydash@users.noreply.github.com>
2021-09-05 23:19:10 +0900

    make code more concise

--- src/main/scala/leetcode/codingchallenge2021/september/OrderlyQueue.scala
+++ src/main/scala/leetcode/codingchallenge2021/september/OrderlyQueue.scala
@@ -4,15 +4,12 @@ object OrderlyQueue {
   def orderlyQueue(s: String, k: Int): String = {
     if (k > 1) s.sorted
     else {
-      val buffer = new StringBuilder(s)
       var result = s
       val minChar = s.min
-      for (i <- 1 until s.length if s(i) == minChar) {
-        for (j <- 0 until (s.length - i)) buffer(j) = s(i + j)
-        for (j <- 0 until i) buffer(s.length - i + j) = s(j)
-        val tmp = buffer.toString()
-        if (tmp < result) result = tmp
-      }
+      for {
+        i <- 1 until s.length if s(i) == minChar
+        tmp = s.substring(i) + s.substring(0, i) if tmp < result
+      } result = tmp
       result
     }
   }
