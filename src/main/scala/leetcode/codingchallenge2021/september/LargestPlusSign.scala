148843c733746ee091edb7620dc5794c2c2950ec
ydash <52343777+ydash@users.noreply.github.com>
2021-09-10 01:18:26 +0900

    remove unneccessary comment

--- src/main/scala/leetcode/codingchallenge2021/september/LargestPlusSign.scala
+++ src/main/scala/leetcode/codingchallenge2021/september/LargestPlusSign.scala
@@ -20,37 +20,4 @@ object LargestPlusSign {
 
     (for (i <- 0 until n; j <- 0 until n) yield up(i)(j) min down(i)(j) min left(i)(j) min right(i)(j)).max
   }
-//    val dp = Array.fill(n)(Array.fill(n)(-1))
-//    for (i <- 0 until n) {
-//      dp(i)(0) = 1
-//      dp(i)(n - 1) = 1
-//      dp(0)(i) = 1
-//      dp(n - 1)(i) = 1
-//    }
-//
-//    for (Array(x, y) <- mines) dp(x)(y) = 0
-//
-//    dp.foreach(it => println(it.mkString("\t,")))
-//
-//    def count(x: Int, y: Int): Int =
-//      if (x < 0 || y < 0 || x >= n || y >= n) 0
-//      else
-//        dp(x)(y) match {
-//          case v if v >= 0 => v
-//          case _ =>
-//            val u = count(x - 1, y)
-//            val l = count(x, y - 1)
-//            val r = count(x, y + 1)
-//            val d = count(x + 1, y)
-//            ((u min l min r min d) + 1).tap(dp(x)(y) = _)
-//        }
-//
-//    var ans = 0
-//    for (i <- 0 until n; j <- 0 until n) {
-//      println(s"$i,$j")
-//      ans = ans max count(i, j)
-//    }
-//
-//    ans
-//  }
 }
