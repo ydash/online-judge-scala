82efe08a71ce366fb8d6f87b6f7ae47558a5cc6b
ydash <52343777+ydash@users.noreply.github.com>
2021-09-10 02:04:28 +0900

    improve runtime and memory usage

--- src/main/scala/leetcode/codingchallenge2021/september/LargestPlusSign.scala
+++ src/main/scala/leetcode/codingchallenge2021/september/LargestPlusSign.scala
@@ -2,22 +2,24 @@ package leetcode.codingchallenge2021.september
 
 object LargestPlusSign {
   def orderOfLargestPlusSign(n: Int, mines: Array[Array[Int]]): Int = {
-    val up, down, left, right = Array.fill(n)(Array.fill(n)(1))
+    val dp = Array.fill(n)(Array.fill(n)(n))
 
     for (Array(x, y) <- mines) {
-      up(x)(y) = 0
-      down(x)(y) = 0
-      left(x)(y) = 0
-      right(x)(y) = 0
+      dp(x)(y) = 0
     }
 
-    for (i <- 1 until n - 1; j <- 1 until n - 1) {
-      if (up(i)(j) != 0) up(i)(j) = up(i - 1)(j) + 1
-      if (down(n - i - 1)(j) != 0) down(n - i - 1)(j) = down(n - i)(j) + 1
-      if (left(j)(i) != 0) left(j)(i) = left(j)(i - 1) + 1
-      if (right(j)(n - i - 1) != 0) right(j)(n - i - 1) = right(j)(n - i) + 1
+    for (i <- 0 until n) {
+      var u, d, l, r = 0
+      for (j <- 0 until n; k = n - j - 1) {
+        dp(j)(i) = dp(j)(i) min { u = if (dp(j)(i) == 0) 0 else u + 1; u }
+        dp(k)(i) = dp(k)(i) min { d = if (dp(k)(i) == 0) 0 else d + 1; d }
+        dp(i)(j) = dp(i)(j) min { l = if (dp(i)(j) == 0) 0 else l + 1; l }
+        dp(i)(k) = dp(i)(k) min { r = if (dp(i)(k) == 0) 0 else r + 1; r }
+      }
     }
 
-    (for (i <- 0 until n; j <- 0 until n) yield up(i)(j) min down(i)(j) min left(i)(j) min right(i)(j)).max
+    var res = 0
+    for (i <- 0 until n; j <- 0 until n) res = res max dp(i)(j)
+    res
   }
 }
