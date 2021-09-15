e9f34aa650e5bf49430c3f6666f38d72cbdcc415
ydash <52343777+ydash@users.noreply.github.com>
2021-09-16 02:42:58 +0900

    make code concise

--- src/main/scala/leetcode/codingchallenge2021/september/LongestTurbulentSubarray.scala
+++ src/main/scala/leetcode/codingchallenge2021/september/LongestTurbulentSubarray.scala
@@ -1,23 +1,18 @@
 package leetcode.codingchallenge2021.september
 
 object LongestTurbulentSubarray {
-  def maxTurbulenceSize(arr: Array[Int]): Int =
-    if (arr.length == 1) 1
-    else {
-      var max, count = 1
-      var slope = -arr(1) + arr(0)
-      for (i <- 1 until arr.length) {
-        val current = arr(i) - arr(i - 1)
-        if (slope < 0 && current > 0 || slope > 0 && current < 0) {
-          count += 1
-        } else {
-          max = max max count
-          count = if (arr(i) == arr(i - 1)) 1 else 2
-        }
-        slope = current
+  def maxTurbulenceSize(arr: Array[Int]): Int = {
+    var answer = 1
+    var head = 0
+    for (i <- 1 until arr.length) {
+      val slope = arr(i) compare arr(i - 1)
+      if (slope == 0) head = i
+      else if (i == arr.length - 1 || slope * (arr(i + 1) compare arr(i)) >= 0) {
+        answer = answer max (i - head + 1)
+        head = i
       }
-
-      max max count
     }
+    answer
+  }
 
 }
