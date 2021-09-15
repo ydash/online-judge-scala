959f72c629a32b0dfca21262543fc92762c17ad2
ydash <52343777+ydash@users.noreply.github.com>
2021-09-16 02:43:15 +0900

    add testcase

--- src/test/scala/leetcode/codingchallenge2021/september/LongestTurbulentSubarraySpec.scala
+++ src/test/scala/leetcode/codingchallenge2021/september/LongestTurbulentSubarraySpec.scala
@@ -9,7 +9,8 @@ class LongestTurbulentSubarraySpec extends LeetCodeSpec {
     (Array(1, 1, 1, 1, 1), 1),
     (Array(4, 8, 12, 16), 2),
     (Array(9, 4, 2, 10, 7, 8, 8, 1, 9), 5),
-    (Array(0, 8, 45, 88, 48, 68, 28, 55, 17, 24), 8)
+    (Array(0, 8, 45, 88, 48, 68, 28, 55, 17, 24), 8),
+    (Array(0, 1, 0, 1, 1), 4)
   )
 
   test("Test Longest Turbulent Subarray") {
