813aec46761492b34fffe4b777a2a6a0a9d84cea
ydash <52343777+ydash@users.noreply.github.com>
2021-09-16 02:13:31 +0900

    add test of longest turbulent subarray

---
+++ src/test/scala/leetcode/codingchallenge2021/september/LongestTurbulentSubarraySpec.scala
@@ -0,0 +1,20 @@
+package leetcode.codingchallenge2021.september
+
+import leetcode.LeetCodeSpec
+
+class LongestTurbulentSubarraySpec extends LeetCodeSpec {
+  private val table = Table(
+    ("arr", "expected"),
+    (Array(100), 1),
+    (Array(1, 1, 1, 1, 1), 1),
+    (Array(4, 8, 12, 16), 2),
+    (Array(9, 4, 2, 10, 7, 8, 8, 1, 9), 5),
+    (Array(0, 8, 45, 88, 48, 68, 28, 55, 17, 24), 8)
+  )
+
+  test("Test Longest Turbulent Subarray") {
+    forAll(table) { (arr, expected) =>
+      LongestTurbulentSubarray.maxTurbulenceSize(arr) shouldBe expected
+    }
+  }
+}
