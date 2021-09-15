bdbe567adc1fcb591a324992b70d313177299be9
ydash <52343777+ydash@users.noreply.github.com>
2021-09-16 02:13:38 +0900

    add solution of longest turbulent subarray

---
+++ src/main/scala/leetcode/codingchallenge2021/september/LongestTurbulentSubarray.scala
@@ -0,0 +1,25 @@
+package leetcode.codingchallenge2021.september
+
+object LongestTurbulentSubarray {
+  def maxTurbulenceSize(arr: Array[Int]): Int =
+    if (arr.length == 1) 1
+    else {
+      var max, count = 1
+      var slope = -arr(1) + arr(0)
+      for (i <- 1 until arr.length) {
+        val current = arr(i) - arr(i - 1)
+        if (slope < 0 && current > 0 || slope > 0 && current < 0) {
+          count += 1
+        } else {
+          max = max max count
+          count = if (arr(i) == arr(i - 1)) 1 else 2
+        }
+        slope = current
+      }
+
+      max max count
+    }
+
+//  def isTurbulent(prev: Int, current: Int, next: Int): Boolean =
+//    (prev < current && current > next) || (prev > current && current < next)
+}
