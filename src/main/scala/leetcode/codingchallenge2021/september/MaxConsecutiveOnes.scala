218d380d14828c5a27e18709b9b75d615dc840ad
ydash <52343777+ydash@users.noreply.github.com>
2021-09-21 21:46:35 +0900

    make code more concise

--- src/main/scala/leetcode/codingchallenge2021/september/MaxConsecutiveOnes.scala
+++ src/main/scala/leetcode/codingchallenge2021/september/MaxConsecutiveOnes.scala
@@ -1,13 +1,11 @@
 package leetcode.codingchallenge2021.september
 
 object MaxConsecutiveOnes {
-  def findMaxConsecutiveOnes(nums: Array[Int]): Int = findMaxLengthOfConsecutiveOnes(nums.toList)
-
-  @scala.annotation.tailrec
-  def findMaxLengthOfConsecutiveOnes(lst: List[Int], acc: Int = 0): Int = lst match {
-    case Nil => acc
-    case _ =>
-      val (ones, rest) = lst.span(_ == 1)
-      findMaxLengthOfConsecutiveOnes(rest.dropWhile(_ == 0), acc max ones.length)
-  }
+  def findMaxConsecutiveOnes(nums: Array[Int]): Int =
+    nums
+      .foldLeft(0 -> 0) {
+        case ((acc, max), 1) => (acc + 1, max max acc + 1)
+        case ((_, max), _)   => (0, max)
+      }
+      ._2
 }
