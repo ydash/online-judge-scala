06bcfe04aa48c0ce0b9a855504fe9ed6cc5e1582
ydash <52343777+ydash@users.noreply.github.com>
2021-09-23 03:32:48 +0900

    improve runtime and memory usage

--- src/main/scala/leetcode/codingchallenge2021/september/MaximumLengthOfAConcatenatedStringWithUniqueCharacters.scala
+++ src/main/scala/leetcode/codingchallenge2021/september/MaximumLengthOfAConcatenatedStringWithUniqueCharacters.scala
@@ -1,21 +1,27 @@
 package leetcode.codingchallenge2021.september
 
+import scala.collection.BitSet
 import scala.collection.mutable.ArrayBuffer
 
 object MaximumLengthOfAConcatenatedStringWithUniqueCharacters {
   def maxLength(lst: List[String]): Int = {
-    val buffer = ArrayBuffer(Set.empty[Char])
+    var answer = 0
+    val buffer = ArrayBuffer(BitSet())
 
     for (str <- lst) {
-      val s1 = str.toSet
-      if (s1.size == str.length) {
+      val b1 = str.foldLeft(BitSet())((acc, c) => acc + (c - 'a'))
+      if (b1.size == str.length) {
         for (i <- buffer.indices) {
-          val s2 = buffer(i)
-          if ((s1 intersect s2).isEmpty) buffer += (s1 union s2)
+          val b2 = buffer(i)
+          if ((b1 & b2).isEmpty) {
+            val concatenated = b1 | b2
+            buffer += concatenated
+            answer = answer max concatenated.size
+          }
         }
       }
     }
 
-    buffer.maxBy(_.size).size
+    answer
   }
 }
