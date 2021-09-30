8ccada275a3613382a6d280548afebfeebf33c7e
ydash <52343777+ydash@users.noreply.github.com>
2021-09-30 21:59:33 +0900

    improve runtime

--- src/main/scala/leetcode/codingchallenge2021/september/PartitionToKEqualSumSubsets.scala
+++ src/main/scala/leetcode/codingchallenge2021/september/PartitionToKEqualSumSubsets.scala
@@ -1,25 +1,29 @@
 package leetcode.codingchallenge2021.september
 
+import scala.collection.mutable.ArrayBuffer
+
 object PartitionToKEqualSumSubsets {
   def canPartitionKSubsets(nums: Array[Int], k: Int): Boolean = {
     val sum = nums.sum
     val subsetSum = sum / k
     if (sum % k != 0 || nums.exists(_ > subsetSum)) false
     else {
-      backtracking(nums.toList, subsetSum, new Array(k))
+      loop(nums.toList.sortBy(-_), subsetSum, k, ArrayBuffer())
     }
   }
 
-  private def backtracking(nums: List[Int], sum: Int, acc: Array[Int]): Boolean = nums match {
-    case Nil => acc.forall(_ == sum)
+  private def loop(nums: List[Int], sum: Int, k: Int, acc: ArrayBuffer[Int]): Boolean = nums match {
+    case Nil => true
     case h :: t =>
-      for (i <- acc.indices) {
-        val s = acc(i)
-        if (s + h <= sum) {
-          acc(i) = s + h
-          if (backtracking(t, sum, acc)) return true
-          acc(i) = s
-        }
+      for (i <- acc.indices if acc(i) + h <= sum) {
+        acc(i) += h
+        if (loop(t, sum, k, acc)) return true
+        acc(i) -= h
+      }
+      if (acc.length < k) {
+        acc += h
+        if (loop(t, sum, k, acc)) return true
+        acc.remove(acc.length - 1)
       }
       false
   }
--- src/test/scala/leetcode/codingchallenge2021/september/PartitionToKEqualSumSubsetsSpec.scala
+++ src/test/scala/leetcode/codingchallenge2021/september/PartitionToKEqualSumSubsetsSpec.scala
@@ -6,7 +6,9 @@ class PartitionToKEqualSumSubsetsSpec extends LeetCodeSpec {
   private val table = Table(
     ("nums", "k", "expected"),
     (Array(4, 3, 2, 3, 5, 2, 1), 4, true),
-    (Array(1, 2, 3, 4), 3, false)
+    (Array(1, 2, 3, 4), 3, false),
+    (Array(3522, 181, 521, 515, 304, 123, 2512, 312, 922, 407, 146, 1932, 4037, 2646, 3871, 269), 5, true),
+    (Array(709, 374, 1492, 1279, 2848, 6337, 365, 1923, 739, 1904, 1938, 4627, 1106, 5885, 1202), 4, true)
   )
 
   test("Test of Partition to K Equal Sum Subsets") {
