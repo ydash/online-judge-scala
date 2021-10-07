9f31dd41ac2873da0ef290a53297eb8fc128c211
ydash <52343777+ydash@users.noreply.github.com>
2021-10-07 23:40:25 +0900

    improve runtime

--- src/main/scala/leetcode/codingchallenge2021/october/WordSearch.scala
+++ src/main/scala/leetcode/codingchallenge2021/october/WordSearch.scala
@@ -1,31 +1,30 @@
 package leetcode.codingchallenge2021.october
 
-import scala.collection.mutable
-
 object WordSearch {
   def exist(board: Array[Array[Char]], word: String): Boolean = {
     val m = board.length
     val n = board.head.length
     val chars = word.toList
-    for (i <- board.indices; j <- board(i).indices if board(i)(j) == chars.head) {
-      if (backtracking(chars.drop(1), i: Int, j: Int, mutable.Set(i -> j))) return true
+    for (i <- board.indices; j <- board(i).indices) {
+      if (backtracking(chars, i, j)) return true
     }
 
-    def backtracking(chars: List[Char], i: Int, j: Int, visited: mutable.Set[(Int, Int)]): Boolean =
+    def backtracking(chars: List[Char], i: Int, j: Int): Boolean =
       chars match {
         case Nil => true
         case c :: tail =>
-          for {
-            (x, y) <- Array((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1))
-            if x < m && x >= 0 && y < n && y >= 0
-            if board(x)(y) == c
-            if !visited.contains(x -> y)
-          } {
-            visited += (x -> y)
-            if (backtracking(tail, x, y, visited)) return true
-            visited -= (x -> y)
+          if (i >= m || i < 0 || j >= n || j < 0 || board(i)(j) != c) false
+          else {
+            val tmp = board(i)(j)
+            board(i)(j) = '.'
+            if (
+              backtracking(tail, i - 1, j) || backtracking(tail, i + 1, j) ||
+              backtracking(tail, i, j - 1) || backtracking(tail, i, j + 1)
+            ) return true
+
+            board(i)(j) = tmp
+            false
           }
-          false
       }
 
     false
