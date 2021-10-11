ef9abe937fc4e619492c58546690efc16933321e
ydash <52343777+ydash@users.noreply.github.com>
2021-10-11 22:23:05 +0900

    make code concise

--- src/main/scala/leetcode/codingchallenge2021/october/DiameterOfBinaryTree.scala
+++ src/main/scala/leetcode/codingchallenge2021/october/DiameterOfBinaryTree.scala
@@ -2,16 +2,14 @@ package leetcode.codingchallenge2021.october
 
 import util.TreeNode
 
-import scala.util.chaining.scalaUtilChainingOps
-
 object DiameterOfBinaryTree {
   def diameterOfBinaryTree(root: TreeNode): Int = {
     def dfs(node: TreeNode): (Int, Int) =
-      if (node == null) (0, -1)
+      if (node == null) (0, 0)
       else {
-        val (ld, ll) = dfs(node.left).pipe { case (a, b) => a -> (b + 1) }
-        val (rd, rl) = dfs(node.right).pipe { case (a, b) => a -> (b + 1) }
-        (ld max rd max (ll + rl), ll max rl)
+        val (lDiam, lLen) = dfs(node.left)
+        val (rDiam, rLen) = dfs(node.right)
+        (lDiam max rDiam max (lLen + rLen), (lLen max rLen) + 1)
       }
 
     dfs(root)._1
