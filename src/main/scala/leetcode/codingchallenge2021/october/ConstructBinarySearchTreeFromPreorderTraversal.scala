4de844a3d7c3e9d4ffebe3098c36ce3adbd31a0c
ydash <52343777+ydash@users.noreply.github.com>
2021-10-13 21:57:33 +0900

    make solution more concise

--- src/main/scala/leetcode/codingchallenge2021/october/ConstructBinarySearchTreeFromPreorderTraversal.scala
+++ src/main/scala/leetcode/codingchallenge2021/october/ConstructBinarySearchTreeFromPreorderTraversal.scala
@@ -2,25 +2,15 @@ package leetcode.codingchallenge2021.october
 
 import util.TreeNode
 
-import scala.collection.mutable
-
 object ConstructBinarySearchTreeFromPreorderTraversal {
-  def bstFromPreorder(preorder: Array[Int]): TreeNode = {
-    val root = new TreeNode(preorder(0))
-    val stack = mutable.Stack[TreeNode](root)
-    for (i <- 1 until preorder.length) {
-      var parent = stack.pop()
-      val child = new TreeNode(preorder(i))
-      if (preorder(i) < parent.value) {
-        parent.left = child
-        parent +=: stack
-        child +=: stack
-      } else {
-        while (stack.nonEmpty && stack.head.value < preorder(i)) parent = stack.pop()
-        parent.right = child
-        child +=: stack
-      }
+  def bstFromPreorder(preorder: Array[Int]): TreeNode = bstFromPreorder(preorder.toList)
+
+  private def bstFromPreorder(preorder: List[Int]): TreeNode =
+    if (preorder.isEmpty) null
+    else {
+      val v = preorder.head
+      val (left, right) = preorder.tail.partition(_ < v)
+      new TreeNode(v, bstFromPreorder(left), bstFromPreorder(right))
     }
-    root
-  }
+
 }
