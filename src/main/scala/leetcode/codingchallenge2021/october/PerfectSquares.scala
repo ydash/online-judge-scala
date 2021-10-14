2799cf149f6a7f2c870cd5b44dbbed4a2ecd0893
ydash <52343777+ydash@users.noreply.github.com>
2021-10-14 21:19:17 +0900

    improve runtime and memory usage

--- src/main/scala/leetcode/codingchallenge2021/october/PerfectSquares.scala
+++ src/main/scala/leetcode/codingchallenge2021/october/PerfectSquares.scala
@@ -3,9 +3,12 @@ package leetcode.codingchallenge2021.october
 object PerfectSquares {
   def numSquares(n: Int): Int = {
     val dp = Array.tabulate(n + 1) { i => i }
-    val perfects = LazyList.from(2).map(i => i * i).takeWhile(_ <= n)
-    for (i <- dp.indices; p <- perfects if i + p <= n) {
-      dp(i + p) = dp(i + p) min (dp(i) + 1)
+    for (i <- dp.indices) {
+      var j = 2
+      while (j * j <= i) {
+        dp(i) = dp(i) min (dp(i - j * j) + 1)
+        j += 1
+      }
     }
     dp.last
   }
