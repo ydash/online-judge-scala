db1878e18c3ddc0dfb9a6f2cf6d660c011fd4364
ydash <52343777+ydash@users.noreply.github.com>
2021-10-23 22:30:17 +0900

    improve memory usage

--- src/main/scala/leetcode/codingchallenge2021/october/FindMinimumInRotatedSortedArrayII.scala
+++ src/main/scala/leetcode/codingchallenge2021/october/FindMinimumInRotatedSortedArrayII.scala
@@ -1,27 +1,19 @@
 package leetcode.codingchallenge2021.october
 
-import scala.collection.mutable.ArrayBuffer
-
 object FindMinimumInRotatedSortedArrayII {
   def findMin(nums: Array[Int]): Int = {
-    var seen = Set[Int]()
-    val uniqNums = ArrayBuffer[Int]()
-
-    for (n <- nums if !seen(n)) {
-      seen += n
-      uniqNums += n
-    }
 
     @scala.annotation.tailrec
     def binSearch(left: Int, right: Int): Int =
       if (left >= right) left
       else {
         val mid = (left + right) / 2
-        if (uniqNums(mid) > uniqNums(right)) binSearch(mid + 1, right)
-        else binSearch(left, mid)
+        if (nums(mid) > nums(right)) binSearch(mid + 1, right)
+        else if (nums(mid) < nums(right)) binSearch(left, mid)
+        else binSearch(left, right - 1)
       }
 
-    uniqNums(binSearch(0, uniqNums.length - 1))
+    nums(binSearch(0, nums.length - 1))
   }
 
 }
