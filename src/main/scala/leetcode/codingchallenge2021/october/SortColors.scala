da754da1f747dd33d0c71c6db4e96d5a43122561
ydash <52343777+ydash@users.noreply.github.com>
2021-10-28 00:36:23 +0900

    improve memory usage

--- src/main/scala/leetcode/codingchallenge2021/october/SortColors.scala
+++ src/main/scala/leetcode/codingchallenge2021/october/SortColors.scala
@@ -1,38 +1,61 @@
 package leetcode.codingchallenge2021.october
 
-import scala.collection.mutable
-
 object SortColors {
   def sortColors(nums: Array[Int]): Unit = {
-    val red, white, blue = mutable.ArrayDeque[Int]()
-    for (i <- nums.indices) {
-      nums(i) match {
-        case 0 => red += i
-        case 1 => white += i
-        case 2 => blue += i
-      }
-      if (red.nonEmpty) {
-        val i = red.removeHead()
-        if (white.nonEmpty) {
-          val j = white.removeHead()
-          nums(j) = 0; nums(i) = 1
-          white += i
-        } else if (blue.nonEmpty) {
-          val k = blue.removeHead()
-          nums(k) = 0; nums(i) = 2
-          blue += i
-        }
-      }
-      if (white.nonEmpty) {
-        if (blue.nonEmpty && white.last > blue.head) {
-          val j = white.removeLast()
-          val k = blue.removeHead()
-          nums(k) = 1; nums(j) = 2
-          white += k; blue += j
-        }
+
+    def swap(i: Int, j: Int): Unit = {
+      val tmp = nums(i)
+      nums(i) = nums(j)
+      nums(j) = tmp
+    }
+
+    var left, current = 0
+    var right = nums.length - 1
+    while (current <= right) {
+      nums(current) match {
+        case 0 =>
+          swap(left, current)
+          current += 1; left += 1
+        case 1 => current += 1
+        case _ =>
+          swap(current, right)
+          right -= 1
       }
     }
   }
+
+  /* This is my previous one-pass solution
+    def sortColors(nums: Array[Int]): Unit = {
+      val red, white, blue = mutable.ArrayDeque[Int]()
+      for (i <- nums.indices) {
+        nums(i) match {
+          case 0 => red += i
+          case 1 => white += i
+          case 2 => blue += i
+        }
+        if (red.nonEmpty) {
+          val i = red.removeHead()
+          if (white.nonEmpty) {
+            val j = white.removeHead()
+            nums(j) = 0; nums(i) = 1
+            white += i
+          } else if (blue.nonEmpty) {
+            val k = blue.removeHead()
+            nums(k) = 0; nums(i) = 2
+            blue += i
+          }
+        }
+        if (white.nonEmpty) {
+          if (blue.nonEmpty && white.last > blue.head) {
+            val j = white.removeLast()
+            val k = blue.removeHead()
+            nums(k) = 1; nums(j) = 2
+            white += k; blue += j
+          }
+        }
+      }
+    }
+   */
 
   /* This is two-pass solution.
   def sortColors(nums: Array[Int]): Unit = {
