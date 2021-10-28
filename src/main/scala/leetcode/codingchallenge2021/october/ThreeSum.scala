3e935a23871555609b302ef16d617f558b85ede9
ydash <52343777+ydash@users.noreply.github.com>
2021-10-28 23:28:00 +0900

    improve runtime

--- src/main/scala/leetcode/codingchallenge2021/october/ThreeSum.scala
+++ src/main/scala/leetcode/codingchallenge2021/october/ThreeSum.scala
@@ -3,16 +3,25 @@ package leetcode.codingchallenge2021.october
 object ThreeSum {
   def threeSum(nums: Array[Int]): List[List[Int]] = {
     nums.sortInPlace()
-    var result = Set[List[Int]]()
-    for (i <- nums.indices.dropRight(2)) {
+    var result = List[List[Int]]()
+    for (
+      i <- nums.indices.dropRight(2).takeWhile(nums(_) <= 0)
+      if i == 0 || nums(i) != nums(i - 1)
+    ) {
       var j = i + 1
       var k = nums.length - 1
       while (j < k) {
-        val sum = nums(i) + nums(j) + nums(k)
-        if (sum == 0) result += List(nums(i), nums(j), nums(k))
-        if (sum <= 0) j += 1 else k -= 1
+        nums(i) + nums(j) + nums(k) match {
+          case 0 =>
+            result = List(nums(i), nums(j), nums(k)) :: result
+            while (j < k && nums(j) == nums(j + 1)) j += 1
+            while (j < k && nums(k) == nums(k - 1)) k -= 1
+            j += 1; k -= 1
+          case sum if sum < 0 => j += 1
+          case _              => k -= 1
+        }
       }
     }
-    result.toList
+    result
   }
 }
