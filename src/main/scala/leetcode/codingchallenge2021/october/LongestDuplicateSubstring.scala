6d8080b5988d1f88cd04befb8ebb952713824919
ydash <52343777+ydash@users.noreply.github.com>
2021-10-31 21:22:48 +0900

    fix my solution by using Rabin Karp

--- src/main/scala/leetcode/codingchallenge2021/october/LongestDuplicateSubstring.scala
+++ src/main/scala/leetcode/codingchallenge2021/october/LongestDuplicateSubstring.scala
@@ -3,11 +3,19 @@ package leetcode.codingchallenge2021.october
 object LongestDuplicateSubstring {
   def longestDupSubstring(s: String): String = {
     def findK(k: Int): Option[String] = {
-      var seen = Set[String]()
-      for (i <- s.indices.dropRight(k - 1)) {
-        val substring = s.substring(i, i + k)
-        if (seen contains substring) return Some(substring)
-        seen += substring
+      val base = 31
+      val mod = Long.MaxValue
+      val p = BigInt(base).modPow(k, mod)
+      var hash = (0 until k).foldLeft(BigInt(0))((acc, i) => ((acc * base) + (s(i) - 'a')) % mod)
+      var cache = Set(hash)
+      var i = k
+      while (i < s.length) {
+        hash = ((hash * base + (s(i) - 'a') - (s(i - k) - 'a') * p) % mod + mod) % mod
+        if (cache.contains(hash)) {
+          if (cache contains hash) return Some(s.substring(i - k + 1, i + 1))
+        }
+        cache = cache + hash
+        i += 1
       }
       None
     }
