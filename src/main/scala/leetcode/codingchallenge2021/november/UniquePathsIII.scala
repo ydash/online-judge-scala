d5005f6f8884b3b8e8da3c6fd9404623b733f16b
ydash <52343777+ydash@users.noreply.github.com>
2021-11-02 19:14:56 +0900

    improve memory usage

--- src/main/scala/leetcode/codingchallenge2021/november/UniquePathsIII.scala
+++ src/main/scala/leetcode/codingchallenge2021/november/UniquePathsIII.scala
@@ -6,26 +6,24 @@ object UniquePathsIII {
     val n = grid.head.length
     val haveToPass = grid.foldLeft(0) { (acc, r) => r.foldLeft(acc) { (a, c) => if (c != -1) a + 1 else a } }
     val (si, sj) = (for (i <- 0 until m; j <- 0 until n if grid(i)(j) == 1) yield (i, j)).head
-
     var count = 0
-    val seen = Array.ofDim[Boolean](m, n)
-    seen(si)(sj) = true
 
     def backtracking(i: Int, j: Int, passed: Int): Unit = {
       if (grid(i)(j) == 2) {
         if (passed == haveToPass) count += 1
       } else {
+        grid(i)(j) = -1
         for (
           (x, y) <- List((i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1))
-          if x >= 0 && x < m && y >= 0 && y < n && grid(x)(y) != -1 && !seen(x)(y)
+          if x >= 0 && x < m && y >= 0 && y < n && grid(x)(y) != -1
         ) {
-          seen(x)(y) = true
           backtracking(x, y, passed + 1)
-          seen(x)(y) = false
         }
+        grid(i)(j) = 0
       }
     }
 
+    grid(si)(sj) = -1
     backtracking(si, sj, 1)
     count
   }
