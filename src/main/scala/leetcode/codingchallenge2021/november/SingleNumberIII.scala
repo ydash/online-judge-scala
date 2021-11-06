167ebb0883d9a1cceaa97fbf854eb92b2537e52d
ydash <52343777+ydash@users.noreply.github.com>
2021-11-06 18:38:35 +0900

    improve runtime complexity

--- src/main/scala/leetcode/codingchallenge2021/november/SingleNumberIII.scala
+++ src/main/scala/leetcode/codingchallenge2021/november/SingleNumberIII.scala
@@ -3,18 +3,15 @@ package leetcode.codingchallenge2021.november
 // 260. Single Number III
 object SingleNumberIII {
   def singleNumber(nums: Array[Int]): Array[Int] = {
-    nums.sortInPlace()
-    var result: List[Int] = Nil
-    var i = 0
-    while (i < nums.length - 1) {
-      if (nums(i) == nums(i + 1)) {
-        i += 2
-      } else {
-        result = nums(i) :: result
-        i += 1
-      }
+    var diff = 0
+    for (n <- nums) diff ^= n
+    diff &= -diff
+
+    val result = Array(0, 0)
+    for (n <- nums) {
+      if ((n & diff) == 0) result(0) ^= n
+      else result(1) ^= n
     }
-    if (nums.length - 1 == i) result = nums.last :: result
-    result.toArray
+    result
   }
 }
