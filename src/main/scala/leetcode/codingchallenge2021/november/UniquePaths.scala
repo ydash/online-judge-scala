b646a1e215b59a4c1033dc59c72573aadb1b1e35
ydash <52343777+ydash@users.noreply.github.com>
2021-11-17 21:31:15 +0900

    improve memory usage: o(n) -> o(1)

--- src/main/scala/leetcode/codingchallenge2021/november/UniquePaths.scala
+++ src/main/scala/leetcode/codingchallenge2021/november/UniquePaths.scala
@@ -1,14 +1,9 @@
 package leetcode.codingchallenge2021.november
 
 object UniquePaths {
-  def uniquePaths(m: Int, n: Int): Int = {
-    val dp = new Array[Int](n + 1)
-    dp(1) = 1
-    for (_ <- 0 until m) {
-      for (j <- 1 to n) {
-        dp(j) = dp(j) + dp(j - 1)
-      }
-    }
-    dp(n)
+  def uniquePaths(_m: Int, _n: Int): Int = {
+    val m = (_m max _n) - 1
+    val n = (_m min _n) - 1
+    (m + 1 to m + n).zip(1 to n).foldLeft(1L) { case (acc, (i, j)) => acc * i / j }.toInt
   }
 }
