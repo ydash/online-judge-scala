b181554f549f6ad4f103cf000d95399bb941e462
ydash <52343777+ydash@users.noreply.github.com>
2021-11-21 20:46:38 +0900

    improve time complexity

--- src/main/scala/leetcode/codingchallenge2021/november/ConstructBinaryTreeFromInorderAndPostorderTraversal.scala
+++ src/main/scala/leetcode/codingchallenge2021/november/ConstructBinaryTreeFromInorderAndPostorderTraversal.scala
@@ -4,13 +4,13 @@ import util.TreeNode
 
 object ConstructBinaryTreeFromInorderAndPostorderTraversal {
   def buildTree(inorder: Array[Int], postorder: Array[Int]): TreeNode = {
-    def build(l: Int, r: Int, pi: Int): TreeNode =
+    val indexOfInorder = inorder.indices.foldLeft(Map[Int, Int]())((acc, i) => acc + (inorder(i) -> i))
+
+    def build(l: Int, r: Int, i: Int): TreeNode =
       if (l > r) null
       else {
-        val m = inorder.indexOf(postorder(pi), l)
-        val rNode = build(m + 1, r, pi - 1)
-        val lNode = build(l, m - 1, pi - (r - m) - 1)
-        new TreeNode(inorder(m), lNode, rNode)
+        val m = indexOfInorder(postorder(i))
+        new TreeNode(inorder(m), build(l, m - 1, i - (r - m) - 1), build(m + 1, r, i - 1))
       }
 
     build(0, inorder.length - 1, postorder.length - 1)
