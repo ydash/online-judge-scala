d83004392b8ef61e78f4a3dbc2e4d9ecae883065
ydash <52343777+ydash@users.noreply.github.com>
2021-12-03 01:49:49 +0900

    make code more concise

--- src/main/scala/leetcode/codingchallenge2021/december/P328.scala
+++ src/main/scala/leetcode/codingchallenge2021/december/P328.scala
@@ -5,25 +5,17 @@ import util.ListNode
 // 328. Odd Even Linked List
 object P328 {
   def oddEvenList(head: ListNode): ListNode = {
-    if (head == null || head.next == null) head
+    if (head == null) head
     else {
-      var current = head.next.next
       var oddCur = head
       val evenRoot = head.next
       var evenCur = evenRoot
-      var i = 1
-      while (current != null) {
-        if (i % 2 != 0) {
-          oddCur.next = current
-          oddCur = current
-        } else {
-          evenCur.next = current
-          evenCur = current
-        }
-        current = current.next
-        i += 1
+      while (evenCur != null && evenCur.next != null) {
+        oddCur.next = evenCur.next
+        oddCur = oddCur.next
+        evenCur.next = oddCur.next
+        evenCur = evenCur.next
       }
-      evenCur.next = null
       oddCur.next = evenRoot
       head
     }
