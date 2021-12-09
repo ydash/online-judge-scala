b9f4efea074252e2c0f07d5c2b6fb04f6a84dee2
ydash <52343777+ydash@users.noreply.github.com>
2021-12-09 22:09:47 +0900

    make code concise

--- src/main/scala/leetcode/codingchallenge2021/december/P1306.scala
+++ src/main/scala/leetcode/codingchallenge2021/december/P1306.scala
@@ -3,13 +3,11 @@ package leetcode.codingchallenge2021.december
 // 1306. Jump Game III
 object P1306 {
   def canReach(arr: Array[Int], start: Int): Boolean = {
-    val visited = new Array[Boolean](arr.length)
     def dfs(i: Int): Boolean =
-      if (i < 0 || i >= arr.length || visited(i)) false
-      else if (arr(i) == 0) true
+      if (i < 0 || i >= arr.length || arr(i) < 0) false
       else {
-        visited(i) = true
-        dfs(i - arr(i)) || dfs(i + arr(i))
+        arr(i) = -arr(i)
+        arr(i) == 0 || dfs(i - arr(i)) || dfs(i + arr(i))
       }
 
     dfs(start)
