dd1a3cef40065dd61c219b0f13e0f4c898910f44
ydash <52343777+ydash@users.noreply.github.com>
2021-12-11 00:54:15 +0900

    improve time complexity: O(n^2) -> O(n)

--- src/main/scala/leetcode/codingchallenge2021/december/P790.scala
+++ src/main/scala/leetcode/codingchallenge2021/december/P790.scala
@@ -4,13 +4,11 @@ package leetcode.codingchallenge2021.december
 object P790 {
   def numTilings(n: Int): Int = {
     val mod = (1e9 + 7).toInt
-    val dp = new Array[Long](n + 1)
+    val dp = new Array[Long](1000)
     dp(0) = 1
-    for (i <- 0 until n) {
-      if (i + 1 <= n) dp(i + 1) = (dp(i + 1) + dp(i)) % mod
-      if (i + 2 <= n) dp(i + 2) = (dp(i + 2) + dp(i)) % mod
-      for (j <- i + 3 to n) dp(j) = (dp(j) + 2 * dp(i)) % mod
-    }
-    dp.last.toInt
+    dp(1) = 2
+    dp(2) = 5
+    for (i <- 3 until n) dp(i) = (2 * dp(i - 1) + dp(i - 3)) % mod
+    dp(n - 1).toInt
   }
 }
