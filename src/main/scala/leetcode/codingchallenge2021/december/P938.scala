ef49abd7c32417e89c0393e337e114d29300c4f6
ydash <52343777+ydash@users.noreply.github.com>
2021-12-15 00:30:57 +0900

    add solution of Range Sum of BST

---
+++ src/main/scala/leetcode/codingchallenge2021/december/P938.scala
@@ -0,0 +1,13 @@
+package leetcode.codingchallenge2021.december
+
+import util.TreeNode
+
+// 938. Range Sum of BST
+object P938 {
+  def rangeSumBST(root: TreeNode, low: Int, high: Int): Int = {
+    if (root == null) 0
+    else if (root.value < low) rangeSumBST(root.right, low, high)
+    else if (root.value > high) rangeSumBST(root.left, low, high)
+    else root.value + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
+  }
+}
