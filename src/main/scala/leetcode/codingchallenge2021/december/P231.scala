5885af81c511959ba3e314041f2f7509d1368de7
ydash <52343777+ydash@users.noreply.github.com>
2021-12-21 22:37:25 +0900

    improve runtime and memory usage

--- src/main/scala/leetcode/codingchallenge2021/december/P231.scala
+++ src/main/scala/leetcode/codingchallenge2021/december/P231.scala
@@ -2,8 +2,5 @@ package leetcode.codingchallenge2021.december
 
 // 231. Power of Two
 object P231 {
-  def isPowerOfTwo(n: Int): Boolean = {
-    def powOfTwo(x: Long = 1): LazyList[Long] = x #:: powOfTwo(x << 1)
-    powOfTwo().dropWhile(_ < n).head == n
-  }
+  def isPowerOfTwo(n: Int): Boolean = n > 0 && (n & (n - 1)) == 0
 }
