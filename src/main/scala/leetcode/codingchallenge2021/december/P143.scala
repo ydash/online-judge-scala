84638738c8db7e6a5c09b64db1b5da5bef52e591
ydash <52343777+ydash@users.noreply.github.com>
2021-12-23 20:35:53 +0900

    improve memory usage

--- src/main/scala/leetcode/codingchallenge2021/december/P143.scala
+++ src/main/scala/leetcode/codingchallenge2021/december/P143.scala
@@ -2,28 +2,37 @@ package leetcode.codingchallenge2021.december
 
 import util.ListNode
 
-import scala.collection.mutable
-
 // 143. Reorder List
 object P143 {
   def reorderList(head: ListNode): Unit = {
-    val deque = mutable.ArrayDeque[ListNode]()
-    var current = head
+    val len = length(head)
+    val mid = len / 2
+    var p1 = head
+    var p2 = head
+    for (_ <- 0 until mid) p2 = p2.next
+    p2 = reverse(p2)
+    while (p1 != p2) {
+      val tmp = p1.next
+      p1.next = p2
+      p1 = p2
+      p2 = tmp
+    }
+  }
+  @scala.annotation.tailrec
+  private def length(head: ListNode, acc: Int = 0): Int =
+    if (head == null) acc
+    else length(head.next, acc + 1)
+
+  def reverse(head: ListNode): ListNode = {
+    var current = head.next
+    var p = head
+    p.next = null
     while (current != null) {
-      deque += current
-      current = current.next
+      val next = current.next
+      current.next = p
+      p = current
+      current = next
     }
-    var p = deque.removeHead()
-    while (deque.nonEmpty) {
-      val l = deque.removeLast()
-      p.next = l
-      p = l
-      val opt = deque.removeHeadOption()
-      opt.foreach { h =>
-        p.next = h
-        p = h
-      }
-      p.next = null
-    }
+    p
   }
 }
