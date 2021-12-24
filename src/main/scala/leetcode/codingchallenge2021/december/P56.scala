997b66f9564b078e89c24bacc0373b2e63217a71
ydash <52343777+ydash@users.noreply.github.com>
2021-12-25 00:16:58 +0900

    make code more concise

--- src/main/scala/leetcode/codingchallenge2021/december/P56.scala
+++ src/main/scala/leetcode/codingchallenge2021/december/P56.scala
@@ -1,18 +1,17 @@
 package leetcode.codingchallenge2021.december
 
+// 56. Merge Intervals
 object P56 {
-  def merge(intervals: Array[Array[Int]]): Array[Array[Int]] = {
-    val result = collection.mutable.ArrayBuffer[Array[Int]]()
-    intervals.sortInPlaceBy(_(0))
-    var current = intervals.head
-    for (interval @ Array(s, e) <- intervals) {
-      if (s <= current(1)) current = Array(current(0) min s, current(1) max e)
-      else {
-        result += current
-        current = interval
+  def merge(intervals: Array[Array[Int]]): Array[Array[Int]] =
+    intervals
+      .sortBy(_(0))
+      .foldLeft(List[Array[Int]]()) { case (acc, interval @ Array(s1, e1)) =>
+        acc match {
+          case Nil                               => interval :: acc
+          case Array(s2, e2) :: tail if s1 <= e2 => Array(s1 min s2, e1 max e2) :: tail
+          case _                                 => interval :: acc
+        }
       }
-    }
-    result += current
-    result.toArray
-  }
+      .reverse
+      .toArray
 }
