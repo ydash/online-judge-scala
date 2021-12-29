3a935e11a2621a69fa701223cc43989f5a721fea
ydash <52343777+ydash@users.noreply.github.com>
2021-12-29 17:31:15 +0900

    improve memory usage

--- src/main/scala/leetcode/codingchallenge2021/december/P116.scala
+++ src/main/scala/leetcode/codingchallenge2021/december/P116.scala
@@ -2,22 +2,18 @@ package leetcode.codingchallenge2021.december
 
 object P116 {
   def connect(root: Node): Node =
-    if (root == null) null
+    if (root == null) root
     else {
-      val queue = new collection.mutable.Queue[Node]()
-      queue += root
-      var numOfNode = 1
-      while (queue.nonEmpty) {
-        var current: Node = null
-        for (_ <- 0 until numOfNode) {
-          val node = queue.dequeue()
-          if (current != null) current.next = node
-          current = node
-          if (node.left != null) {
-            queue += (node.left, node.right)
-          }
+      var headOfNextRow = root
+      var current: Node = null
+      while (headOfNextRow.left != null) {
+        current = headOfNextRow
+        while (current != null) {
+          current.left.next = current.right
+          if (current.next != null) current.right.next = current.next.left
+          current = current.next
         }
-        numOfNode <<= 1
+        headOfNextRow = headOfNextRow.left
       }
       root
     }
