96cd6962cfae010dfb5328ff1a52d528911caea8
ydash <52343777+ydash@users.noreply.github.com>
2022-01-02 21:43:47 +0900

    rename the value

--- src/main/scala/leetcode/codingchallenge2022/jan/P1010.scala
+++ src/main/scala/leetcode/codingchallenge2022/jan/P1010.scala
@@ -3,11 +3,11 @@ package leetcode.codingchallenge2022.jan
 // 1010. Pairs of Songs With Total Durations Divisible by 60
 object P1010 {
   def numPairsDivisibleBy60(time: Array[Int]): Int = {
-    val dp = new Array[Int](60)
+    val cache = new Array[Int](60)
     var answer = 0
     for (t <- time) {
-      answer += dp((60 - t % 60) % 60)
-      dp(t % 60) += 1
+      answer += cache((60 - t % 60) % 60)
+      cache(t % 60) += 1
     }
     answer
   }
