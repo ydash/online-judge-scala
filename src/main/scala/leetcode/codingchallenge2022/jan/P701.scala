795f3d490701c3b2d4620b449ccc1c4ad71e9c1f
ydash <52343777+ydash@users.noreply.github.com>
2022-01-12 21:51:05 +0900

    improve runtime and memory usage

--- src/main/scala/leetcode/codingchallenge2022/jan/P701.scala
+++ src/main/scala/leetcode/codingchallenge2022/jan/P701.scala
@@ -5,11 +5,15 @@ import util.TreeNode
 // 701. Insert into a Binary Search Tree
 class P701 {
   def insertIntoBST(root: TreeNode, n: Int): TreeNode = {
-    if (root == null) new TreeNode(n)
-    else {
-      if (root.value > n) root.left = insertIntoBST(root.left, n)
-      else root.right = insertIntoBST(root.right, n)
-      root
+    if (root == null) return new TreeNode(n)
+    var current, parent = root
+    while (current != null) {
+      parent = current
+      if (current.value > n) current = current.left
+      else current = current.right
     }
+    if (parent.value > n) parent.left = new TreeNode(n)
+    else parent.right = new TreeNode(n)
+    root
   }
 }
