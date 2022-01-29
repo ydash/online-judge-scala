df9098bda474bbb9c56e8a6607c41a6c8ba8a2a4
ydash <52343777+ydash@users.noreply.github.com>
2022-01-29 18:54:22 +0900

    improve runtime and memory usage

--- src/main/scala/leetcode/codingchallenge2022/jan/P84.scala
+++ src/main/scala/leetcode/codingchallenge2022/jan/P84.scala
@@ -1,16 +1,23 @@
 package leetcode.codingchallenge2022.jan
 
 object P84 {
-  def largestRectangleArea(seqOfHistogram: Array[Int]): Int = {
-    val highest = seqOfHistogram.max
-    val dp = new Array[Int](highest + 1)
+  def largestRectangleArea(height: Array[Int]): Int = {
+    var stack = List(-1)
     var result = 0
-    for (h <- seqOfHistogram) {
-      for (i <- 1 to h) {
-        dp(i) = dp(i) + i
-        result = result max dp(i)
+    for (i <- height.indices) {
+      while (stack.head != -1 && height(i) < height(stack.head)) {
+        val h = height(stack.head)
+        stack = stack.tail
+        val w = i - stack.head - 1
+        result = result max (h * w)
       }
-      for (i <- h + 1 to highest) dp(i) = 0
+      stack = i :: stack
+    }
+    while (stack.head != -1) {
+      val h = height(stack.head)
+      stack = stack.tail
+      val w = height.length - stack.head - 1
+      result = result max (h * w)
     }
     result
   }
