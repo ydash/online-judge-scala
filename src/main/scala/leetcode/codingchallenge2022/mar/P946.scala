ceb190c42b9e5261edd16e70e18a81aa3f5da0b6
ydash <52343777+ydash@users.noreply.github.com>
2022-03-16 20:12:54 +0900

    improve runtime complexity: O(n^2) -> O(n)

--- src/main/scala/leetcode/codingchallenge2022/mar/P946.scala
+++ src/main/scala/leetcode/codingchallenge2022/mar/P946.scala
@@ -3,16 +3,15 @@ package leetcode.codingchallenge2022.mar
 // 946. Validate Stack Sequences
 object P946 {
   def validateStackSequences(pushed: Array[Int], popped: Array[Int]): Boolean = {
-    val indexOfN = pushed.indices.foldLeft(Map[Int, Int]()) { (acc, i) => acc + (pushed(i) -> i) }
-    for (pi <- popped.indices) {
-      val i = indexOfN(popped(pi))
-      var minIndex = i
-      for (pj <- pi + 1 until popped.length) {
-        val j = indexOfN(popped(pj))
-        if (j < i && j > minIndex) return false
-        minIndex = minIndex min j
+    val stack = collection.mutable.Stack[Int]()
+    var i = 0
+    for (n <- pushed) {
+      stack.push(n)
+      while (stack.nonEmpty && stack.head == popped(i)) {
+        stack.pop()
+        i += 1
       }
     }
-    true
+    stack.isEmpty
   }
 }
