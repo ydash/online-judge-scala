3001746a089e3e2a271f5c77189e8e7b939330da
ydash <52343777+ydash@users.noreply.github.com>
2022-03-19 11:53:15 +0900

    imporve runtime complexity: O(nlog(n)) -> O(n)

--- src/main/scala/leetcode/codingchallenge2022/mar/P895.scala
+++ src/main/scala/leetcode/codingchallenge2022/mar/P895.scala
@@ -7,20 +7,22 @@ import scala.collection.mutable
 
 class FreqStack() {
   private val freq = mutable.Map[Int, Int]().withDefaultValue(0)
-  private val countMap = mutable.SortedMap[Int, mutable.Stack[Int]]()
+  private val countMap = mutable.Map[Int, mutable.Stack[Int]]()
+  private var maxFreq = 0
 
   def push(value: Int): Unit = {
     freq(value) += 1
     countMap
       .get(freq(value))
       .fold[Unit](countMap += (freq(value) -> mutable.Stack(value)))(_.push(value))
+    maxFreq = maxFreq max freq(value)
   }
 
   def pop(): Int = {
-    val stack = countMap(countMap.lastKey)
+    val stack = countMap(maxFreq)
     val result = stack.pop()
     freq(result) -= 1
-    if (stack.isEmpty) countMap -= countMap.lastKey
+    if (stack.isEmpty) maxFreq -= 1
     result
   }
 
