c452c049c342e574668d983babf735f10e5ca1dc
ydash <52343777+ydash@users.noreply.github.com>
2022-03-21 14:51:32 +0900

    make code concise

--- src/main/scala/leetcode/codingchallenge2022/mar/P763.scala
+++ src/main/scala/leetcode/codingchallenge2022/mar/P763.scala
@@ -4,17 +4,15 @@ package leetcode.codingchallenge2022.mar
 object P763 {
   def partitionLabels(s: String): List[Int] = {
     val lastIndexOf = s.indices.foldLeft(Map[Char, Int]()) { (acc, i) => acc + (s(i) -> i) }
-    var current = 0
+    var maxInd, len = 0
     val buffer = collection.mutable.ListBuffer[Int]()
-    while (current < s.length) {
-      val startInd = current
-      var lastInd = lastIndexOf(s(current))
-      do {
-        val tmp = lastInd
-        lastInd = (current to lastInd).map(i => lastIndexOf(s(i))).max
-        current = tmp + 1
-      } while (current <= lastInd)
-      buffer += (lastInd - startInd + 1)
+    for (i <- s.indices) {
+      len += 1
+      maxInd = maxInd max lastIndexOf(s(i))
+      if (maxInd <= i) {
+        buffer += len
+        len = 0
+      }
     }
     buffer.result()
   }
