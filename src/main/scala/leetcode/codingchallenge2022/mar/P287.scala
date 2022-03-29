4dd15b9e0d1d028b5d06eb9915672ba2fd6fb5f6
ydash <52343777+ydash@users.noreply.github.com>
2022-03-29 23:21:42 +0900

    improve runtime complexity: O(nlogn) -> O(n)

--- src/main/scala/leetcode/codingchallenge2022/mar/P287.scala
+++ src/main/scala/leetcode/codingchallenge2022/mar/P287.scala
@@ -3,16 +3,16 @@ package leetcode.codingchallenge2022.mar
 // 287. Find the Duplicate Number
 object P287 {
   def findDuplicate(nums: Array[Int]): Int = {
-    @scala.annotation.tailrec
-    def binSearch(left: Int, right: Int): Int = {
-      if (left >= right) left
-      else {
-        val mid = (left + right) / 2
-        val count = nums.count(_ <= mid)
-        if (count > mid) binSearch(left, mid)
-        else binSearch(mid + 1, right)
-      }
+    var slow, fast = nums.head
+    do {
+      slow = nums(slow)
+      fast = nums(nums(fast))
+    } while (slow != fast)
+    slow = nums.head
+    while (slow != fast) {
+      slow = nums(slow)
+      fast = nums(fast)
     }
-    binSearch(1, nums.length - 1)
+    slow
   }
 }
