bcbb35628f36cf603a56b9db036fb5ae7e0d8e05
ydash <52343777+ydash@users.noreply.github.com>
2022-04-12 22:59:19 +0900

    improve space complexity: O(mn)->O(1)

--- src/main/scala/leetcode/codingchallenge2022/apr/P289.scala
+++ src/main/scala/leetcode/codingchallenge2022/apr/P289.scala
@@ -4,24 +4,17 @@ package leetcode.codingchallenge2022.apr
 object P289 {
   def gameOfLife(board: Array[Array[Int]]): Unit = {
     def get(arr: Array[Array[Int]], i: Int, j: Int): Int = {
-      arr.lift(i).flatMap(_.lift(j)).getOrElse(0)
+      arr.lift(i).flatMap(_.lift(j)).getOrElse(0) % 10
     }
     def countNeighbor(arr: Array[Array[Int]], i: Int, j: Int): Int = {
       get(arr, i - 1, j - 1) + get(arr, i - 1, j) + get(arr, i - 1, j + 1) +
         get(arr, i, j - 1) + get(arr, i, j + 1) +
         get(arr, i + 1, j - 1) + get(arr, i + 1, j) + get(arr, i + 1, j + 1)
     }
-    val prev = Array.tabulate(board.length)(board(_).clone())
     for (i <- board.indices; j <- board(i).indices) {
-      val env = countNeighbor(prev, i, j)
-      if (prev(i)(j) == 1) {
-        board(i)(j) =
-          if (env < 2) 0
-          else if (env <= 3) 1
-          else 0
-      } else {
-        if (env == 3) board(i)(j) = 1
-      }
+      val env = countNeighbor(board, i, j)
+      if (board(i)(j) == 1 && (env == 2 || env == 3) || board(i)(j) == 0 && env == 3) board(i)(j) += 10
     }
+    for (i <- board.indices; j <- board(i).indices) board(i)(j) /= 10
   }
 }
