5589d4e0055e86c8443f1090de3d6c18735f93aa
ydash <52343777+ydash@users.noreply.github.com>
2022-05-03 00:09:14 +0900

    add solution of Sort Array By Parity

---
+++ src/main/scala/leetcode/codingchallenge2022/apr/P905.scala
@@ -0,0 +1,19 @@
+package leetcode.codingchallenge2022.apr
+
+// 905. Sort Array By Parity
+object P905 {
+  def sortArrayByParity(nums: Array[Int]): Array[Int] = {
+    @scala.annotation.tailrec
+    def sort(_i: Int, _j: Int, arr: Array[Int]): Array[Int] = {
+      val i = arr.indexWhere(_ % 2 == 1, _i)
+      val j = arr.lastIndexWhere(_ % 2 == 0, _j)
+      if (i != -1 && j != -1 && i < j) {
+        val tmp = arr(i)
+        arr(i) = arr(j)
+        arr(j) = tmp
+        sort(i, j, arr)
+      } else arr
+    }
+    sort(0, nums.length - 1, nums)
+  }
+}
---
+++ src/test/scala/leetcode/codingchallenge2022/apr/P905Test.scala
@@ -0,0 +1,19 @@
+package leetcode.codingchallenge2022.apr
+
+import leetcode.LeetCodeSpec
+
+class P905Test extends LeetCodeSpec {
+  private val table = Table(
+    ("nums", "expected"),
+    (Array(3, 1, 2, 4), Array(4, 2, 1, 3)),
+    (Array(1, 3), Array(1, 3)),
+    (Array(2), Array(2)),
+    (Array(1, 2, 3, 4), Array(4, 2, 3, 1))
+  )
+
+  test("Test of Sort Array By Parity") {
+    forAll(table) { (nums, expected) =>
+      P905.sortArrayByParity(nums) shouldBe expected
+    }
+  }
+}
