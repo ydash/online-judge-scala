53bb2602e9f7995e3e0ba589c23efa597fd26c3b
ydash <52343777+ydash@users.noreply.github.com>
2022-05-04 00:47:31 +0900

    improve runtime complexity: O(nlogn) -> O(n)

--- src/main/scala/leetcode/codingchallenge2022/may/P581.scala
+++ src/main/scala/leetcode/codingchallenge2022/may/P581.scala
@@ -3,9 +3,16 @@ package leetcode.codingchallenge2022.may
 // 581. Shortest Unsorted Continuous Subarray
 object P581 {
   def findUnsortedSubarray(nums: Array[Int]): Int = {
-    val sorted = nums.sorted
-    val start = nums.indices.find(i => nums(i) != sorted(i))
-    val end = nums.indices.findLast(j => nums(j) != sorted(j))
-    start.flatMap(i => end.map(j => j - i + 1)).getOrElse(0)
+    val n = nums.length
+    @scala.annotation.tailrec
+    def loop(i: Int, start: Int, end: Int, maxFromLeft: Int, minFromRight: Int): Int = {
+      if (i >= n) end - start + 1
+      else {
+        val max = maxFromLeft max nums(i)
+        val min = minFromRight min nums(n - i - 1)
+        loop(i + 1, if (nums(n - i - 1) > min) n - i - 1 else start, if (nums(i) < max) i else end, max, min)
+      }
+    }
+    loop(0, -1, -2, nums.head, nums.last)
   }
 }
