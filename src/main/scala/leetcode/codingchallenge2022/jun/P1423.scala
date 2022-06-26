a79098cd47a1cae44e48f72752957f53ddd66779
ydash <52343777+ydash@users.noreply.github.com>
2022-06-26 15:53:31 +0900

    improve memory complexity: O(k) -> O(1)

--- src/main/scala/leetcode/codingchallenge2022/jun/P1423.scala
+++ src/main/scala/leetcode/codingchallenge2022/jun/P1423.scala
@@ -3,14 +3,11 @@ package leetcode.codingchallenge2022.jun
 // 1423. Maximum Points You Can Obtain from Cards
 object P1423 {
   def maxScore(cardPoints: Array[Int], k: Int): Int = {
-    val sumFromLeft, sumFromRight = new Array[Int](k + 1)
-    for (i <- 1 to k) {
-      sumFromLeft(i) = sumFromLeft(i - 1) + cardPoints(i - 1)
-      sumFromRight(i) = sumFromRight(i - 1) + cardPoints(cardPoints.length - i)
-    }
-    var result = 0
-    for (i <- 0 to k) {
-      result = result max (sumFromLeft(i) + sumFromRight(k - i))
+    var result, currentSum = cardPoints.takeRight(k).sum
+    for (i <- 0 until k) {
+      currentSum += cardPoints(i)
+      currentSum -= cardPoints(cardPoints.length - k + i)
+      result = result max currentSum
     }
     result
   }
